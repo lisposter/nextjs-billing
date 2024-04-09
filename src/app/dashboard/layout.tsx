44f3835bc8d31a9a588e6785330589cdf6a86984
Leigh <i@zhuli.me>
2024-04-09 09:39:10 +0800

    feat: supabase auth

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,16 +2,25 @@ import type { Metadata } from "next";
 import Script from "next/script";
 import { Sidebar } from "@/components/dashboard/sidebar";
 import { Toaster } from "@/components/toaster";
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/lib/supabase/server'
 
 export const metadata: Metadata = {
   title: "Dashboard | Lemon Squeezy Next.js Billing Template",
 };
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const supabase = createClient()
+
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    redirect('/')
+  }
   return (
     <>
       {/* Load the Lemon Squeezy's Lemon.js script before the page is interactive. */}
@@ -22,6 +31,7 @@ export default function DashboardLayout({
 
       <div className="grid h-lvh grid-cols-[270px_1fr] text-sm leading-6 text-surface-500">
         <Sidebar />
+        {data.user.email}
         {children}
       </div>
       <Toaster position="bottom-right" />
